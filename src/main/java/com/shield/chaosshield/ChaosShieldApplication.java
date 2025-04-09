073a76b2ad48e5d2590bf179427879636ba722b9
buausb <11656672+buausb@user.noreply.gitee.com>
2025-04-09 20:06:20 +0800

    启动类

--- src/main/java/com/shield/chaosshield/ChaosShieldApplication.java
+++ src/main/java/com/shield/chaosshield/ChaosShieldApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ChaosShieldApplication {
-
+// 执行入口在Server.class
     public static void main(String[] args) {
         SpringApplication.run(ChaosShieldApplication.class, args);
     }
